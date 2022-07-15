23d52c1309e6b6b6001fb80463429e2f8a38427b
WALVREIVE <bolottultemirovkg@gmail.com>
2022-07-15 11:48:40 +0600

    hero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,16 +12,17 @@ public class Main {
     h1.setDamage(25);
     h1.setSuperAbility("TELEPORT");{
    Hero h2 = new Hero();
-            System.out.println("SECO STATISTICS : " + h1.getHealth() + " is - health" + h1.getDamage()
+            System.out.println("SECOND STATISTICS : " + h1.getHealth() + " is - health. " + h1.getDamage()
                     + " is - damage");
    h2.setHealth(15);
    h2.setDamage(-1000);
    Boss b1 = new Boss();
-            System.out.println("STATISTICS OF BOSS: " + b1.getBossHp() + " is - Boss HP" + b1.getBossDamage() + " is - Boss Damage" + b1.getAttackType() + " is - ATTACK TYPE");
    b1.setBossHp(500);
    b1.setBossDamage(100);
    b1.setAttackType("MAGIC");
-    }
+            System.out.println("STATISTICS OF BOSS: " + b1.getBossHp() + " is - Boss HP. " + b1.getBossDamage() + " is - Boss Damage. " + b1.getAttackType() + " is - ATTACK TYPE .");
+
+        }
     }
 
 
